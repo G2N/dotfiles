77e06648b6a587ae7613922f91f75d2b2a6d39c0
Guillaume DENORMANDIE <g.denormandie@topachat.com>
2025-02-20 10:15:33 +0100

    tac variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,3 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-# overwrite greeting
-# potentially disabling fastfetch
 function fish_greeting
     # smth smth
 end
@@ -10,6 +6,12 @@ end
 alias vi='nvim'
 alias vim='nvim'
 
+source ~/.config/fish/tac_variables.fish
+
 # Enable Starship
 # https://starship.rs
 starship init fish | source
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
---
+++ fish/tac_variables.fish
@@ -0,0 +1,10 @@
+# TAC shortcuts
+set TAC_STARFOX_DIR ~/starfox
+alias starfox="$TAC_STARFOX_DIR/bin/starfox"
+
+set TAC_STARFOX_BUILD_DIR "$TAC_STARFOX_DIR/build"
+set ADMINAPP "$TAC_STARFOX_BUILD_DIR/admin-app/data"
+set BACKEND "$TAC_STARFOX_BUILD_DIR/back-end/data"
+set FRONTEND "$TAC_STARFOX_BUILD_DIR/front-end/data"
+set SVELTE "$TAC_STARFOX_BUILD_DIR/front-endv2/data/svelte"
+set WSOCK "$TAC_STARFOX_BUILD_DIR/front-wsock/data"
