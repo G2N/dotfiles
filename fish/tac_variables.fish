bf5e023f5f28cb8f9223b777a71eb0bf1d6ca3ea
Guillaume Denormandie <g.denormandie@topachat.com>
2026-05-22 14:15:18 +0200

    update tac variables

--- fish/tac_variables.fish
+++ fish/tac_variables.fish
@@ -6,12 +6,9 @@ alias gut="git"
 alias gtu="git"
 
 # TAC shortcuts
-set TAC_STARFOX_DIR ~/starfox
-alias starfox="$TAC_STARFOX_DIR/bin/starfox"
-
-set TAC_STARFOX_BUILD_DIR "$TAC_STARFOX_DIR/build"
-set ADMINAPP "$TAC_STARFOX_BUILD_DIR/admin-app/data"
-set BACKEND "$TAC_STARFOX_BUILD_DIR/back-end/data"
-set FRONTEND "$TAC_STARFOX_BUILD_DIR/front-end/data"
-set SVELTE "$TAC_STARFOX_BUILD_DIR/front-endv2/data/svelte"
-set WSOCK "$TAC_STARFOX_BUILD_DIR/front-wsock/data"
+set TOPACHAT_DIR ~/topachat
+set BASHFOX_DIR "$TOPACHAT_DIR/bashfox"
+set ADMINAPP "$TOPACHAT_DIR/admin-app"
+set BACKEND "$TOPACHAT_DIR/back-end"
+set FRONTEND "$TOPACHAT_DIR/front-end/svelte"
+set WSOCK "$TOPACHAT_DIR/front-wsock"
